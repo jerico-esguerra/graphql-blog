5aa095c57a13cb6751a571182d4135094cd97239
jerico-esguerra <jericoesguerra212@outlook>
2023-01-22 10:12:36 -0500

    add Categories

--- src/services/index.js
+++ src/services/index.js
@@ -39,21 +39,6 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
-export const getCategories = async () => {
-  const query = gql`
-    query GetGategories {
-        categories {
-          name
-          slug
-        }
-    }
-  `;
-
-  const result = await request(graphqlAPI, query);
-
-  return result.categories;
-};
-
 export const getPostDetails = async (slug) => {
   const query = gql`
     query GetPostDetails($slug : String!) {
@@ -128,4 +113,19 @@ export const getRecentPosts = async () => {
   const result = await request(graphqlAPI, query);
 
   return result.posts;
+};
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetGategories {
+        categories {
+          name
+          slug
+        }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
 };
