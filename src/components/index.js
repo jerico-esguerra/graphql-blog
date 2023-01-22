8b07869e3c075cb80805d36ffbcad8459ae61312
jerico-esguerra <jericoesguerra212@outlook>
2023-01-22 14:21:01 -0500

    add styles, Loader, AdjacentPosts

--- src/components/index.js
+++ src/components/index.js
@@ -8,4 +8,5 @@ export { default as Comments } from './Comments';
 export { default as CommentsForm } from './CommentsForm';
 export { default as Loader } from './Loader';
 export { default as Header } from './Header';
-export { default as FeaturedPostCard} from './FeaturedPostCard'
+export { default as FeaturedPostCard} from './FeaturedPostCard';
+export {default as AdjacentPostCard} from './AdjacentPostCard'
