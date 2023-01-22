bc7794e18405c04ff0072d5bba9e9cd0df4bf94d
jerico-esguerra <jericoesguerra212@outlook>
2023-01-22 12:27:27 -0500

    layout and style Author. Fix spelling error

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 import moment from 'moment';
 import Link from 'next/link';
 
-import { grpahCMSImageLoader } from '../util';
+import { graphCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => (
   <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
     {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
         unoptimized
-        loader={grpahCMSImageLoader}
+        loader={graphCMSImageLoader}
         alt={post.title}
         className="shadow-lg rounded-t-lg lg:rounded-lg"
         layout="fill"
@@ -28,7 +28,7 @@ const PostCard = ({ post }) => (
       <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
         <Image
           unoptimized
-          loader={grpahCMSImageLoader}
+          loader={graphCMSImageLoader}
           alt={post.author.name}
           height="30"
           width="30"
