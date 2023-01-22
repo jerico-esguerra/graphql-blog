952ec64197b576336e4cc15df994d095daf75cd8
jerico-esguerra <jericoesguerra212@outlook>
2023-01-22 02:11:43 -0500

    basic Home layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Head from 'next/head';
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const posts = [{title: "Testing"}]
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Head><title>Memories</title></Head>
+      <div className="container mx-auto px-10 mb-8">
+        <div>Featured Posts</div>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="lg:col-span-8 col-span-1">
+            {posts.map((post, index) => (
+              <div>{post.title}</div>
+            ))}
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <div>Post Widget</div>
+              <div>Categories</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
